555f9f5e1f8854cf1ce245c2c38c8f2900ca91bb
aquaman1182 <hal.bandotakumi@gmail.com>
2023-06-19 10:40:42 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -9,3 +9,18 @@ void main() async{
     child: MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: LoginPage(),
+    );
+  }
+}
